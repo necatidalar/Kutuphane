143c13e1bd8187018612beef262b5dfe2e9b30cc
necatidalar <necatidalar5@gmail.com>
2025-12-01 11:27:52 +0300

    .

--- Kutuphane.DAL/KitapRepository.cs
+++ Kutuphane.DAL/KitapRepository.cs
@@ -1,15 +1,6 @@
-﻿using KutuphaneOtomasyonu.Models;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Kutuphane.DAL;
 
-namespace Kutuphane.DAL
+public class KitapRepository
 {
-    public class KitapRepository
-    {
-        
-    }
+    
 }
