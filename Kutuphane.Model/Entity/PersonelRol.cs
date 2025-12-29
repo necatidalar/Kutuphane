3baf5c4e7bfda173283ff0def548f8c20e4d5673
necatidalar <necatidalar5@gmail.com>
2025-12-29 15:32:48 +0300

    .

---
+++ Kutuphane.Model/Entity/PersonelRol.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entity;
+
+namespace Kutuphane.Model.Entity
+{
+    public class PersonelRol : IEntity
+    {
+        public int PersonelId { get; set; }
+        public int RolId { get; set; }
+        
+        public Personel Personel { get; set; }
+        public Rol Rol { get; set; }
+    }
+}
---
+++ Kutuphane.Model/Entity/RolYetki.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entity;
+
+namespace Kutuphane.Model.Entity
+{
+    public class RolYetki : IEntity
+    {
+        public int RolId { get; set; }
+        public int YetkiId { get; set; }
+        
+        public Rol Rol { get; set; }
+        public Yetki Yetki { get; set; }
+    }
+}
